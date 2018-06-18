9eda901732884fd6a83aa12afb7a2e9546fd6444
joonij <37970610+joonij@users.noreply.github.com>
2018-06-18 17:03:01 +0900

    release 20180618

--- js/javascript.js
+++ js/javascript.js
@@ -231,46 +231,110 @@ $(document).ready(function(){
     //    버튼
     var locations = '';
     $("#store_btn div").click(function(){
-        var locations = null;
-        var locations = [];
-        var locations = '';
         $("#store_btn div").animate({right:"-50px"}, 100);
         $(this).animate({right:"0px"}, 100, function location(){
+            locations = [];
             if ($(this).index() === 0) {
-                locations  = [
-                    ['classic_4', 37.664846, 127.043596, 4],
-                    ['classic_3', 37.451964, 126.900737, 3],
-                    ['classic_2', 37.509588, 126.887553, 2],
-                    ['classic_1', 37.639825, 127.068618, 1],
+                var locations  = [
+                    ['classic_4', 37.664846, 127.043596],
+                    ['classic_3', 37.451964, 126.900737],
+                    ['classic_2', 37.509588, 126.887553],
+                    ['classic_1', 37.639825, 127.068618],
                 ];
             } else if ($(this).index() === 1){
-                 locations  = [
-                    ['w_6', 37.559955, 126.840502, 6],
-                    ['w_5', 37.535214, 127.095767, 5],
-                    ['w_4', 37.509271, 127.007485, 4],
-                    ['w_3', 37.498131, 127.026428, 3],
-                    ['w_2', 37.482090, 126.881627, 2],
-                    ['w_1', 37.477736, 127.124954, 1],
+                var locations  = [
+                    ['w_6', 37.559955, 126.840502],
+                    ['w_5', 37.535214, 127.095767],
+                    ['w_4', 37.509271, 127.007485],
+                    ['w_3', 37.498131, 127.026428],
+                    ['w_2', 37.482090, 126.881627],
+                    ['w_1', 37.477736, 127.124954],
                 ];
             } else if ($(this).index() === 2){
-                 locations = [
-                    ['a_lab_1', 37.556232, 126.934529, 1]
+                var locations = [
+                    ['a_lab_1', 37.556232, 126.934529]
                 ];
             } else if ($(this).index() === 3){
-                 locations = [
-                    ['queens_2', 37.511158, 127.098135, 2],
-                    ['queens_1', 35.157189, 129.063093, 1]
+                var locations = [
+                    ['queens_2', 37.511158, 127.098135],
+                    ['queens_1', 35.157189, 129.063093]
                 ]; 
             }
-            for (i = 0; i < locations.length; i++) {  
+//            function clearMarkers() {
+//                setMapOnAll(null);
+//            }
+//            function deleteMarkers() {
+//                clearMarkers();
+//                markers = [];
+//            }
+//            deleteMarkers()
+            for (i = 0; i < locations.length; i++) {
                 marker = new google.maps.Marker({
                     position: new google.maps.LatLng(locations[i][1], locations[i][2]),
                     map: map,
-                    data: {
-                        name: locations[i][0]
-                    }
                 });
             }
+            
+            
+            
+//            // In the following example, markers appear when the user clicks on the map.
+//            // The markers are stored in an array.
+//            // The user can then click an option to hide, show or delete the markers.
+//            var map;
+//            var markers = [];
+//
+//            function initMap() {
+//                var haightAshbury = {lat: 37.566645, lng: 126.978371};
+//
+//                map = new google.maps.Map(document.getElementById('map'), {
+//                    zoom: 11,
+//                    center: haightAshbury,
+//                    mapTypeId: 'terrain'
+//                });
+//
+//                // This event listener will call addMarker() when the map is clicked.
+//                map.addListener('click', function(event) {
+//                    addMarker(event.latLng);
+//                });
+//
+//                // Adds a marker at the center of the map.
+//                addMarker(haightAshbury);
+//            }
+//
+//              // Adds a marker to the map and push to the array.
+//            function addMarker(location) {
+//                var marker = new google.maps.Marker({
+//                    position: location,
+//                    map: map
+//                });
+//                markers.push(marker);
+//            }
+//
+//              // Sets the map on all markers in the array.
+//            function setMapOnAll(map) {
+//                for (var i = 0; i < markers.length; i++) {
+//                    markers[i].setMap(map);
+//                }
+//            }
+//
+//              // Removes the markers from the map, but keeps them in the array.
+//            function clearMarkers() {
+//                setMapOnAll(null);
+//            }
+//
+//            // Shows any markers currently in the array.
+//            function showMarkers() {
+//                setMapOnAll(map);
+//            }
+//
+//            // Deletes all markers in the array by removing references to them.
+//            function deleteMarkers() {
+//                clearMarkers();
+//                markers = [];
+//            }
+            
+            
+            
         });
     });
 });
